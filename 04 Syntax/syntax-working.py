782347c8460ca47f572e50b4cdbc97e2a856b5dd
ACpython <acpython777@gmail.com>
2017-08-08 10:13:08 -0400

    Creating main script

---
+++ 04 Syntax/syntax-working.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#!/usr/bin/python3
+
+# print("Hello, World!")
+
+def main():
+    print("This is the syntax.py file")
+    egg()
+
+def egg():
+    print("egg")
+
+if __name__ == "__main__": main()
