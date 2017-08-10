2ffa57b6a5768c6cb1b580ead74724e9bcd2d51a
ACpython <acpython777@gmail.com>
2017-08-10 15:57:23 -0400

    setting multiple choices with elif

---
+++ 06 Conditionals/conditionals-elif.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# conditionals.py by Bill Weinman [http://bw.org/]
+# This is an exercise file from Python 3 Essential Training on lynda.com
+# Copyright 2010 The BearHeart Group, LLC
+
+def main():
+    # v is one
+    v = 'one'
+    if v == 'one':
+        print('v is one')
+    elif v == 'two':
+        print('v is two')
+    elif v == 'three':
+        print('v is three')
+    else: 
+        print('v is some other thing ')
+
+if __name__ == "__main__": main()
